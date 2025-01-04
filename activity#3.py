86035593ad734f4902265b39453c911cb1486c47
mahad446 <mahadmubashir43@gmail.com>
2025-01-04 14:51:04 -0800

    activity#3

---
+++ activity#3.py
@@ -0,0 +1,14 @@
+# take marks as input from user
+print("enter marks obtained in 4 subjects: ")
+maths = int(input("maths : "))
+english = int(input("english :"))
+science = int(input("science:"))
+urdu = int(input("urdu :"))
+
+# let' s calculate the percentage of marks
+sum = (" sum maths+english+science+urdu")
+
+perc = (sum/400)*100
+
+print(end="percentage marks =")
+print(perc)
